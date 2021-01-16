21a5da4e0f97263c1812c7876ab7a8dd8c2ff6a0
kuuueee <69327018+kuuueee@users.noreply.github.com>
2021-01-16 10:10:10 -0800

    Update country-message.js

--- src/commands/country-message.js
+++ src/commands/country-message.js
@@ -5,9 +5,13 @@ const prefix = config.prefix;
 module.exports = {
 	name: 'country-message',
 	description: 'Sends message to the country of the users choosing notifying which country sent the message',
-	//aliases: ['commands'],
+	aliases: ['message-country', 'message_country','country_message'],
 	usage: '{country receiving message} {message}\n**Example:** '+prefix+'country-message france we will support your move into brussels if you give us passage through belgium',
 	execute(message, args, client) {
+		//if the message came from a dm, exit out of the command without warning.
+		if (message.channel.type === 'dm') {
+			return;
+		}
 		const fs = require('fs')
 		fs.readFile('./json/country_id.json', 'utf8', (err, jsonString) => {
 		    if (err) {
@@ -23,6 +27,7 @@ module.exports = {
 		    		break;
 		    	}
 		    }
+		    args[0] = args[0].toLowerCase();
 		    if (!user_is_power){
 		    	//user is not a power
 		    	message.reply('you are not authorized to make this command.');
@@ -30,11 +35,11 @@ module.exports = {
 		    	//user is a power
 		    	if (args[0] == user_country){
 		    		//user is a power that is sending an country_message to them selves
-		    		message.reply('you may not send an anonymous message to yourself');
+		    		message.reply('you may not send a message to yourself');
 		    	} else if ((args[0] == 'austria' || args[0] == 'england' || args[0] == 'france' || 
 		    			   args[0] == 'germany' || args[0] == 'italy' || args[0] == 'russia' || 
-		    			   args[0] == 'turkey'|| args[0] == 'world') && args[0] != user_country){
-		    		//send an anonymous message to 
+		    			   args[0] == 'turkey') && args[0] != user_country){
+		    		//send a message to 
 			    	for (i = 0; i < country_id.length; i++){
 			    		if (country_id[i].country == args[0]){
 			    			var anon_message_channel = country_id[i].channel_id;
@@ -50,10 +55,10 @@ module.exports = {
 			    	}
 					const channel = client.channels.cache.get(anon_message_channel);
 					channel.send(country_message);
-					message.author.send("Your message has been sent to " + message);
+					message.author.send("Your message has been sent to " + args[0]);
 		    	} else {
 		    		message.reply('your command must include a valid country as the second argument'
-		    		+ '\nie. country_message England attack at dawn');
+		    		+ '\nie. country-message England attack at dawn');
 		    	}
 		    }
 		})
